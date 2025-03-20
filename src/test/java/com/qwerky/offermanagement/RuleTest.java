4d322eec15f9c5862085647cc7d8114c6adfce63
Neil Taylor <uklocalbeer@gmail.com>
2025-03-20 12:00:42 +0000

    price rule test

--- src/test/java/com/qwerky/offermanagement/RuleTest.java
+++ src/test/java/com/qwerky/offermanagement/RuleTest.java
@@ -16,7 +16,6 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.*;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -37,9 +36,6 @@ public class RuleTest {
 
     @Value("${kafka.topics.valid-offers}")
     private String validOffersTopic;
-
-    @Value("${kafka.topics.candidate-offers}")
-    private String candidateOffersTopic;
 
     @Mock
     private KafkaTemplate<String, String> kafkaTemplate;
@@ -105,7 +101,65 @@ public class RuleTest {
         verify(validOfferRepository).deleteById(marketplaceOffer.getId());
 
         // kafkaTemplate should send both offers to the validOffersTopic
-        ArgumentCaptor<String> stringCaptor = ArgumentCaptor.forClass(String.class);
+        verify(kafkaTemplate, times(2)).send(eq(validOffersTopic), any());
+    }
+
+
+    /**
+     * Tests that a new offer with a lower price will replace an existing offer.
+     */
+    @Test
+    public void lowestPriceTest() throws JsonProcessingException {
+        Product product = new Product();
+        product.setEan("5434667");
+
+        Pricing highPrice = new Pricing();
+        highPrice.setCurrentPrice(10.00f);
+        Seller seller1 = new Seller();
+        seller1.setName("ACME supplies");
+        Offer offer1 = new Offer();
+        offer1.setId("highPrice");
+        offer1.setProduct(product);
+        offer1.setSeller(seller1);
+        offer1.setPricing(highPrice);
+
+        Pricing lowPrice = new Pricing();
+        highPrice.setCurrentPrice(5.00f);
+        Seller seller2 = new Seller();
+        seller2.setName("Cheapo merchant");
+        Offer offer2 = new Offer();
+        offer2.setId("lowPrice");
+        offer2.setProduct(product);
+        offer2.setSeller(seller2);
+        offer2.setPricing(lowPrice);
+
+        // Prepare Mocks
+        ArgumentCaptor<Offer> offerCaptor = ArgumentCaptor.forClass(Offer.class);
+
+        // candidateOfferRepository should get a findByEan() with the product EAN, and should return the marketplaceOffer
+        List<Offer> candidateOffers = List.of(offer1, offer2);
+        when(candidateOfferRepository.findByEan(product.getEan())).thenReturn(candidateOffers);
+
+        // validOfferRepository should get a existsById with the high price offer id and return true
+        when(validOfferRepository.existsById(offer1.getId())).thenReturn(true);
+
+        // Execute test
+        String message = new ObjectMapper().writeValueAsString(offer2);
+        eventManager.receive(message);
+
+        // candidateOfferRepository should get a save() with the low price offer
+        verify(candidateOfferRepository).save(offerCaptor.capture());
+        assertEquals(offerCaptor.getValue().getId(), offer2.getId());
+
+        // validOfferRepository should get a save with the low price offer, and the offer should be valid
+        verify(validOfferRepository).save(offerCaptor.capture());
+        assertTrue(offerCaptor.getValue().isValid());
+        assertEquals(offerCaptor.getValue().getId(), offer2.getId());
+
+        // validOfferRepository should get a deleteById with the high price offer id
+        verify(validOfferRepository).deleteById(offer1.getId());
+
+        // kafkaTemplate should send both offers to the validOffersTopic
         verify(kafkaTemplate, times(2)).send(eq(validOffersTopic), any());
     }
 }
